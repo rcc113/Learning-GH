23a3905dce0332c748a40b2ee6fa30822d08930f
Ryan <ryan.c.collins.99@gmail.com>
2023-05-24 18:25:31 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ryan Collins on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
